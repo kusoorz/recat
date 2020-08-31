e8dfdf1029c1c735b822f6b71c068d957cd10a7b
kusoorz <kusoorz@live.com>
2020-08-31 20:00:30 +0800

    更新首页

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { showLoading } from 'store'
+import { withRouter } from 'react-router-dom'
+
+import './index.scss'
 
 class Index extends React.Component {
     componentDidMount () {
-        const { dispatch } = this.props
-        dispatch(showLoading(true))
     }
     render () {
+        const { history } = this.props
         return (
-            <div className="index-view"></div>
+            <div className="index-view">
+                <div className="titlebar">Recat</div>
+                <div className="contents">
+                    <ul>
+                        <li>
+                            <div className="title" onClick={() => history.push('/')}>Toast</div>
+                            <div className="text">信息提示组件</div>
+                            <div className="code">{`<Toast show={show} text="提示!"></Toast>`}</div>
+                        </li>
+                        <li>
+                            <div className="title" onClick={() => history.push('/')}>Dialog</div>
+                            <div className="text">内容弹窗组件</div>
+                            <div className="code">{`<Dialog show={show} click={onclick}>这里是弹窗</Dialog>`}</div>
+                        </li>
+                        <li>
+                            <div className="title" onClick={() => history.push('/')}>Loading</div>
+                            <div className="text">加载提示组件</div>
+                            <div className="code">{`<Loading show={show}></Loading>`}</div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         )
     }
 }
 
-export default connect()(Index)
+export default withRouter(connect()(Index))
