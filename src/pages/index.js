a1ce9debc08b85d5e59e20f87045a0ecee1842fd
kusoorz <kusoorz@live.com>
2020-08-31 20:00:46 +0800

    更新路由配置

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import { lazy } from 'react'
 
+const exact = true
+
 const routes = [{
     name: 'index',
     path: '/',
+    exact,
     component: lazy(() => import('./index/index'))
+}, {
+    name: 'toast',
+    path: '/toast',
+    exact,
+    component: lazy(() => import('./toast/index'))
+}, {
+    name: 'dialog',
+    path: '/dialog',
+    exact,
+    component: lazy(() => import('./dialog/index'))
+}, {
+    name: 'loading',
+    path: '/loading',
+    exact,
+    component: lazy(() => import('./loading/index'))
 }]
 
 export default routes
