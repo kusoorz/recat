1a85e4bc078420a2ae4adee3b1fed96701284cf4
kusoorz <kusoorz@live.com>
2020-08-31 20:00:48 +0800

    更新首页

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -15,19 +15,16 @@ class Index extends React.Component {
                 <div className="contents">
                     <ul>
                         <li>
-                            <div className="title" onClick={() => history.push('/')}>Toast</div>
+                            <div className="title" onClick={() => history.push('/toast')}>Toast</div>
                             <div className="text">信息提示组件</div>
-                            <div className="code">{`<Toast show={show} text="提示!"></Toast>`}</div>
                         </li>
                         <li>
-                            <div className="title" onClick={() => history.push('/')}>Dialog</div>
+                            <div className="title" onClick={() => history.push('/dialog')}>Dialog</div>
                             <div className="text">内容弹窗组件</div>
-                            <div className="code">{`<Dialog show={show} click={onclick}>这里是弹窗</Dialog>`}</div>
                         </li>
                         <li>
-                            <div className="title" onClick={() => history.push('/')}>Loading</div>
+                            <div className="title" onClick={() => history.push('/loading')}>Loading</div>
                             <div className="text">加载提示组件</div>
-                            <div className="code">{`<Loading show={show}></Loading>`}</div>
                         </li>
                     </ul>
                 </div>
