e97f2be52297bd63f75de7b6b563512112aea59a
kusoorz <kusoorz@live.com>
2020-08-31 20:00:10 +0800

    首页路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import { Index } from 'pages'
 import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  )
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Index/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Index from './index/index'
+
+export {
+    Index
+}
